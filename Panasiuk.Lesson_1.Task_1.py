c4f7bad604989082c7acd471263c88c820cf3dd0
UserD <panasiuk74@mail.ru>
2022-10-14 21:41:26 +0300

    добавил запись

---
+++ Panasiuk.Lesson_1.Task_1.py
@@ -0,0 +1,9 @@
+a = 10  # объявили переменную а типа int равную 10
+b = 10.5   # объявили переменную b типа float равную 10.5
+c = True  #объявили переменную с типа bool равную  "True"
+d = "Hello ,teacher"  # обьявили переменную d типа string равную "Hello ,teacher"
+a = str (a)
+
+print(a,b,c,d)
+
+sfgjkk
